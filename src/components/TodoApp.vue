0404bd19a5b9e9dd9b2a2cf02e76d9aa9822e685
Tatsuya Sato <satoryu.1981@gmail.com>
2021-02-08 15:26:59 +0900

    Implemented a tiny todo management application

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <input v-model="inputedTodo" @keypress.enter="addTodo">
+
+  <ul>
+    <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" >
+    </TodoItem>
+  </ul>
+
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  components: { TodoItem },
+  data() {
+    return {
+      inputedTodo: '',
+      todos: [
+        { id: 0, task: 'タスクを入力する' },
+        { id: 1, task: 'Enterを押す' },
+        { id: 2, task: '完了したタスクのチェックを付ける' }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      // If given task is empty, nothing to do
+      if (!this.inputedTodo) {
+        return
+      }
+
+      let newId = Math.max(...(this.todos.map(t => t.id)));
+
+      this.todos.push({id: newId, task: this.inputedTodo});
+      this.inputedTodo = '';
+    }
+  }
+}
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>{{ todo.task }}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object
+  }
+}
+</script>
