3d1b9d6462bc47ade06e9ce79322c9a3491ded25
Tatsuya Sato <satoryu.1981@gmail.com>
2021-02-08 16:21:42 +0900

    Users can mark a task as completed

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -18,9 +18,10 @@ export default {
     return {
       inputedTodo: '',
       todos: [
-        { id: 0, task: 'タスクを入力する' },
-        { id: 1, task: 'Enterを押す' },
-        { id: 2, task: '完了したタスクのチェックを付ける' }
+        { id: 0, task: 'タスクを入力する', completed: false },
+        { id: 1, task: 'Enterを押す', completed: false },
+        { id: 2, task: '完了したタスクのチェックを付ける', completed: false },
+        { id: 3, task: '息を吸う', completed: true }
       ]
     }
   },
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,30 @@
 <template>
-  <li>{{ todo.task }}</li>
+  <li :class="{ completed: completed }">
+    <input type="checkbox" v-model="completed">
+    {{ todo.task }}
+  </li>
 </template>
 
 <script>
 export default {
   props: {
     todo: Object
+  },
+  data() {
+    return {
+      completed: false
+    }
+  },
+  created() {
+    if (this.todo.completed !== undefined) {
+      this.completed = this.todo.completed;
+    }
   }
 }
 </script>
+
+<style scoped>
+.completed {
+  text-decoration-line: line-through;
+}
+</style>
