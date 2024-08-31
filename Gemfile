e533c39076b79b55324f3a0a26f97e601c2330cf
Tatsuya Sato <satoryu.1981@gmail.com>
2024-09-01 02:22:56 +0900

    bootstrapのmodalでmodalで新規投稿画面を追加

--- Gemfile
+++ Gemfile
@@ -46,6 +46,9 @@ gem "bootsnap", require: false
 
 gem 'haml-rails'
 
+gem 'bootstrap', '~> 5.0'
+gem 'bootstrap_form' , '~> 5.3'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.create(task_params)
+
+    render :new unless @task.persisted?
   end
 
   def show
@@ -18,5 +23,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:summary, :description)
   end
 end
