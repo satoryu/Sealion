b57660f87560425a00184ecdf3dc70c358ec2d16
Tatsuya Sato <satoryu.1981@gmail.com>
2025-01-29 01:14:09 +0900

    Add due date validation to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   validates :summary, presence: true
+  validate :due_date_later_than_today
+
+  private
+
+  def due_date_later_than_today
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, 'must be later than today')
+    end
+  end
 end
